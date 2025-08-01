c4d09b5974d7c1299f788b404b00f9d97dcc47e0
Chandranil Adhikary <chandraniladhikary16dec2003@gmail.com>
2025-08-02 00:03:12 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const userrouter=require("./routes/Userroute")
 const chatrouter=require("./routes/Chatroute")
 mongoconnect()
 app.use(cookieParser())
-app.use(cors({origin: "https://nirogya-chatbot.onrender.com", credentials: true, allowedHeaders: ["Content-Type", "Authorization"]}))
+app.use(cors({origin: "https://nirogya-chatbot.onrender.com", credentials: true, allowedHeaders: "*"}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
